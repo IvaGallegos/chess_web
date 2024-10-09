dcb923c39e7da6224b4164fe91ea912d392d5d28
Cristobal Alejandro Soto Mancilla <cri.sotom@duocuc.cl>
2024-10-09 16:56:34 -0300

    descripcion de mis cambios

---
+++ chessproject/chessapp/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'chessapp/home.html')
+
+def login_view(request):
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        # Aquí puedes implementar tu lógica para autenticar con Firebase
+        # Si la autenticación es exitosa
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)  # Inicia sesión en Django
+            return redirect('menu')  # Redirige al menú después de iniciar sesión
+        else:
+            return render(request, 'chessapp/login.html', {'error': 'Credenciales incorrectas'})
+    return render(request, 'chessapp/login.html')
+
+def logout_view(request):
+    logout(request)  
+    return redirect('home') 
+
+def signin(request):
+    return render(request, 'chessapp/signin.html')
+
+def menu(request):
+    return render(request, 'chessapp/menu.html')
+
+def tablero(request):
+    return render(request, 'chessapp/tablero.html')
+
+
+def home(request):
+    return render(request, 'chessapp/home.html')  
+
+def partidas_en_vivo(request):
+    return render(request, 'chessapp/partidas_en_vivo.html') 
+
+def tutoriales(request):
+    return render(request, 'chessapp/tutoriales.html')
+    
+def estadisticas_view(request):
+    return render(request, 'chessapp/estadisticas.html')
+
+
+
+
+
+
+
