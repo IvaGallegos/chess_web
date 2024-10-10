45762cb6c26f13b9abe4270d4aab31b9d5e70574
Angelquidiante <An.quidiante@duocuc.cl>
2024-10-10 20:36:41 -0300

    cambios menores

--- chessproject/chessapp/static/chessapp/accesibilidad.js
+++ chessproject/chessapp/static/chessapp/accesibilidad.js
@@ -1,22 +1,5 @@
-// Al cargar la página, restablecer las preferencias
-window.onload = function() {
-    const userId = "user_id"; // Cambia esto por el ID del usuario actual
-    db.collection("configuraciones").doc(userId).get().then((doc) => {
-        if (doc.exists) {
-            const data = doc.data();
-            if (data.tamanoFuente) {
-                document.body.style.fontSize = data.tamanoFuente;
-            }
 
-            if (data.colorTema) {
-                if (data.colorTema === 'oscuro') {
-                    cambiarColorTema(); // Cambia el tema a oscuro
-                }
-            }
-        }
-    });
-};
-
+//Funcion para cambiar el tamaño de fuente
 function cambiarTamanoFuente(accion) {
     const cuerpo = document.body;
     let tamanoActual = parseFloat(window.getComputedStyle(cuerpo).fontSize);
@@ -26,33 +9,5 @@ function cambiarTamanoFuente(accion) {
     } else if (accion === 'disminuir') {
         cuerpo.style.fontSize = (tamanoActual - 2) + 'px';
     }
-
-    // Guardar el tamaño de fuente en Firestore
-    const userId = "user_id"; // Cambia esto por el ID del usuario actual
-    db.collection("configuraciones").doc(userId).set({
-        tamanoFuente: cuerpo.style.fontSize
-    }, { merge: true });
 }
 
-function cambiarColorTema() {
-    const cuerpo = document.body;
-    if (cuerpo.style.backgroundColor === 'black') {
-        cuerpo.style.backgroundColor = 'white';
-        cuerpo.style.color = 'black';
-
-        // Guardar la preferencia de color claro en Firestore
-        const userId = "user_id"; // Cambia esto por el ID del usuario actual
-        db.collection("configuraciones").doc(userId).set({
-            colorTema: 'claro'
-        }, { merge: true });
-    } else {
-        cuerpo.style.backgroundColor = 'black';
-        cuerpo.style.color = 'white';
-
-        // Guardar la preferencia de color oscuro en Firestore
-        const userId = "user_id"; // Cambia esto por el ID del usuario actual
-        db.collection("configuraciones").doc(userId).set({
-            colorTema: 'oscuro'
-        }, { merge: true });
-    }
-}
