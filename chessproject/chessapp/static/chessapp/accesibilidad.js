400458b3fca4d308cb1539dbdda3c10ffa058188
Angelquidiante <An.quidiante@duocuc.cl>
2024-10-29 13:12:39 -0300

    mejoras visuales a la rama principal

--- chessproject/chessapp/static/chessapp/accesibilidad.js
+++ chessproject/chessapp/static/chessapp/accesibilidad.js
@@ -1,13 +1,26 @@
 
-//Funcion para cambiar el tamaño de fuente
-function cambiarTamanoFuente(accion) {
+// Función para cambiar el tamaño de fuente y el zoom del contenido
+function cambiarTamanoZoom(accion) {
     const cuerpo = document.body;
-    let tamanoActual = parseFloat(window.getComputedStyle(cuerpo).fontSize);
-    
+    let zoomActual = parseFloat(window.getComputedStyle(cuerpo).getPropertyValue('zoom')) || 1; 
+
+    // Ajusta el zoom global de todo el cuerpo
     if (accion === 'aumentar') {
-        cuerpo.style.fontSize = (tamanoActual + 2) + 'px';
+        cuerpo.style.zoom = (zoomActual + 0.1);  // Aumenta el zoom en 10%
     } else if (accion === 'disminuir') {
-        cuerpo.style.fontSize = (tamanoActual - 2) + 'px';
+        cuerpo.style.zoom = (zoomActual - 0.1);  // Disminuye el zoom en 10%
     }
 }
 
+// Función para ocultar el menu de tamaño de fuente y el zoom del contenido
+
+function toggleAccesibilidad() {
+    const accesibilidad = document.getElementById('accesibilidadOptions');
+    if (accesibilidad.style.visibility === 'hidden') {
+        accesibilidad.style.visibility = 'visible'; // Mostrar
+    } else {
+        accesibilidad.style.visibility = 'hidden'; // Ocultar
+    }
+}
+
+    
